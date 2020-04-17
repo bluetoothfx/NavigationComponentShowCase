595430fd836902955b6353ae2f9c9e24ed34db85
Al Abrar <firebold42@gmail.com>
2020-04-17 15:26:02 +0600

    added tabbed layout in fragment

--- app/src/main/java/com/experiment/navigationcomponentshowcase/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/experiment/navigationcomponentshowcase/ui/dashboard/DashboardFragment.kt
@@ -7,25 +7,65 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.experiment.navigationcomponentshowcase.R
+import com.experiment.navigationcomponentshowcase.ui.dashboard.tabbeddesign.FragmentOne
+import com.experiment.navigationcomponentshowcase.ui.dashboard.tabbeddesign.FragmentThree
+import com.experiment.navigationcomponentshowcase.ui.dashboard.tabbeddesign.FragmentTwo
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        /*val textView: TextView = root.findViewById(R.id.text_dashboard)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
-        })
+        })*/
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = MyViewPagerAdapter(childFragmentManager)
+        adapter.addFragment(FragmentOne(), " One ")
+        adapter.addFragment(FragmentTwo(), " Two ")
+        adapter.addFragment(FragmentThree(), " Three ")
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+    }
+
+    class MyViewPagerAdapter(manager: androidx.fragment.app.FragmentManager) :
+        androidx.fragment.app.FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+        private val fragmentList: MutableList<androidx.fragment.app.Fragment> = ArrayList()
+        private val titleList: MutableList<String> = ArrayList()
+
+        override fun getItem(position: Int): androidx.fragment.app.Fragment {
+            return fragmentList[position]
+        }
+
+        override fun getCount(): Int {
+            return fragmentList.size
+        }
+
+        fun addFragment(fragment: androidx.fragment.app.Fragment, title: String) {
+            fragmentList.add(fragment)
+            titleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titleList[position]
+        }
     }
 }
---
+++ app/src/main/java/com/experiment/navigationcomponentshowcase/ui/dashboard/tabbeddesign/FragmentOne.kt
@@ -0,0 +1,21 @@
+package com.experiment.navigationcomponentshowcase.ui.dashboard.tabbeddesign
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.experiment.navigationcomponentshowcase.R
+import kotlinx.android.synthetic.main.fragment_tabbed_layout.*
+
+class FragmentOne : androidx.fragment.app.Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_tabbed_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        tv_fragment_name.text = "Fragment One"
+    }
+}
